e2a4855cf3f299557238ce9e6d0e71e668de7b3e
Omoigui Efosa <omoigui.efosa@thegiggroupng.com>
2021-12-14 12:11:05 +0100

    Test Configure

---
+++ IRIS.BCK.Test/GetAllShipmentListHandlerTest.cs
@@ -0,0 +1,37 @@
+//using AutoMapper;
+//using IRIS.BCK.Application.Interfaces.IRepository;
+//using IRIS.BCK.Core.Domain.Entities.ShimentEntities;
+//using Moq;
+//using System;
+//using Xunit;
+
+//namespace IRIS.BCK.Test
+//{
+//    public class GetAllShipmentListHandlerTest 
+//    {
+//        private readonly IMapper _mapper;
+//        private readonly Mock<IGenericRepository<Shipment>> _mockShipmentRepository;
+
+//        public GetAllShipmentListHandlerTest() 
+//        {
+//            _mockShipmentRepository = RepositoryMocks.;
+//            var configurationProvider = new MapperConfiguration(cfg =>
+//            {
+//                cfg.AddProfile<MappingProfile>();
+//            });
+            
+//            _mapper = mapper;
+//        }
+
+//        public GetAllShipmentListHandlerTest()
+//        {
+
+//        }
+
+//        [Fact]
+//        public void Test1()
+//        {
+
+//        }
+//    }
+//}
---
+++ IRIS.BCK.Test/RepositoryMocks.cs
@@ -0,0 +1,18 @@
+﻿using IRIS.BCK.Application.Interfaces.IRepository;
+using IRIS.BCK.Core.Domain.Entities.ShimentEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRIS.BCK.Test
+{
+    public static class RepositoryMocks
+    {
+        //public static Mock<IGenericRepository<Shipment>> GetShipmentRepository()
+        //{
+        //    return null;
+        //}
+    }
+}
