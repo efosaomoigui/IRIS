eadd055d5cc85bf4ac7474cb869fa5335e963abf
efosaomoigui <efe.omoigui@gmail.com>
2022-01-21 14:48:05 +0100

    Cors Reset

--- IRIS.BCK.Api/Startup.cs
+++ IRIS.BCK.Api/Startup.cs
@@ -92,15 +92,17 @@ namespace IRIS.BCK.Api
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "IRIS.BCK.Api v1"));
             }
 
-            app.UseHttpsRedirection();
-            app.UseRouting();
-
             //app.UseCors("IrisCors"); 
             app.UseCors(x => x
                            .AllowAnyMethod()
+                           .AllowAnyOrigin()
                            .AllowAnyHeader()
                            .SetIsOriginAllowed(origin => true) // allow any origin
                            .AllowCredentials()); // allow credentials
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
             app.UseAuthentication();
             app.UseAuthorization();
 
