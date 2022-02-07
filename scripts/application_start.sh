684dfada858c6e2e7eaa346b45e0ad52e146fa19
ChiscoIris <94366786+ChiscoIris@users.noreply.github.com>
2022-02-07 15:59:08 +0100

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,4 +5,6 @@
  
  cd /home/ubuntu/IRIS.BCK.Api
  sudo dotnet build
+ sudo chown -R ubuntu:ubuntu ./bin
+ sudo chown -R ubuntu:ubuntu ./obj
  pm2 start 'dotnet run  --urls "http://localhost:5000"' --name IRIS
