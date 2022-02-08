b66cdbd6195ff9ef6c3cd6527e76863c1f892cd1
ChiscoIris <94366786+ChiscoIris@users.noreply.github.com>
2022-02-08 07:52:07 +0100

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,6 +4,12 @@
  sudo rm -rf bin/
  sudo rm -rf obj/
  sudo dotnet build
+ sudo chmod -R 777 /home/ubuntu/IRIS.BCK.Api/
+ sudo chown -R ubuntu:ubuntu /home/ubuntu/IRIS.BCK.Api/
+ sudo chmod -R 777 /home/ubuntu/IRIS.BCK.Infrastructure.Files/
+ sudo chown -R ubuntu:ubuntu /home/ubuntu/IRIS.BCK.Infrastructure.Files/
+ sudo chmod -R 777 /home/ubuntu/IRIS.BCK.Infrastructure.Persistence/
+ sudo chown -R ubuntu:ubuntu /home/ubuntu/IRIS.BCK.Infrastructure.Persistence/
  sudo chown -R ubuntu:ubuntu ./bin
  sudo chown -R ubuntu:ubuntu ./obj
  sudo pm2 start 'dotnet run  --urls "http://localhost:5000"' --name IRIS
