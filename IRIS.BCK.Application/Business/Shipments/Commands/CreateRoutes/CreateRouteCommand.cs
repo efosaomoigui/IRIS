f74e5bfedcb6b9a7b66098de3f99955326951745
Chiamaka-Uzuegbu <maureenclassy60@gmail.com>
2022-02-08 13:36:27 +0100

    Update Route Entity

--- IRIS.BCK.Application/Business/Shipments/Commands/CreateRoutes/CreateRouteCommand.cs
+++ IRIS.BCK.Application/Business/Shipments/Commands/CreateRoutes/CreateRouteCommand.cs
@@ -14,8 +14,8 @@ namespace IRIS.BCK.Core.Application.Business.Shipments.Commands.CreateRoutes
 
         public string RouteName { get; set; }
 
-        public int DepartureCentreId { get; set; }
-        public int DestinationCentreId { get; set; }
+        public string Destination { get; set; }
+        public string Departure { get; set; }
         public bool IsSubRoute { get; set; }
 
         public decimal DispatchFee { get; set; }
