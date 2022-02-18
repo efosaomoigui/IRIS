77e83b0f7a220adfae1971c01193e5b67e5f5786
Chiamaka-Uzuegbu <maureenclassy60@gmail.com>
2022-02-18 10:20:27 +0100

    Migrations

--- IRIS.BCK.Infrastructure.Persistence/IRISDbContext.cs
+++ IRIS.BCK.Infrastructure.Persistence/IRISDbContext.cs
@@ -47,7 +47,7 @@ namespace IRIS.BCK.Infrastructure.Persistence
             modelBuilder.Entity<Route>().Property(p => p.CaptainFee).HasColumnType("decimal(18,4)");
             modelBuilder.Entity<Route>().Property(p => p.LoaderFee).HasColumnType("decimal(18,4)");
             modelBuilder.Entity<Route>().Property(p => p.DispatchFee).HasColumnType("decimal(18,4)");
-            modelBuilder.Entity<Shipment>().Property(p => p.DeclarationOfValueCheck).HasColumnType("decimal(18,4)");
+            modelBuilder.Entity<Shipment>().Property(p => p.GrandTotal).HasColumnType("decimal(18,4)");
 
             modelBuilder.Entity<Shipment>()
        .HasMany(a => a.CustomerAddress)
