2729050ce89c0ccd717aae01a42247a502b7ffd4
Chiamaka-Uzuegbu <maureenclassy60@gmail.com>
2022-03-17 13:20:13 +0100

    Create Product Enum

---
+++ IRIS.BCK.Domain/EntityEnums/ProductEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRIS.BCK.Core.Domain.EntityEnums
+{
+    public enum ProductEnum
+    {
+        Tomatoes,
+        Vedan,
+        Noodles,
+        Flour,
+        Cowbell,
+        Nestle,
+        Bigi
+    }
+}
