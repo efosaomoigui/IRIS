9a33028787ed600fc26953290c41cf05f3867c1e
odgwuval1 <valentine.madu@outsourceglobal.com>
2022-04-13 11:16:25 +0100

    add workflow script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
  cd /home/ubuntu/IRIS.BCK/IRIS.BCK.Api
- sudo rm -rf bin/
- sudo rm -rf obj/
- sudo dotnet build || { echo 'build failed' ; exit 1; }
+#  sudo rm -rf bin/
+#  sudo rm -rf obj/
+#  sudo dotnet build || { echo 'build failed' ; exit 1; }
  sudo chmod -R 777 /home/ubuntu/IRIS.BCK/IRIS.BCK.Api/
  sudo chown -R ubuntu:ubuntu /home/ubuntu/IRIS.BCK/IRIS.BCK.Api/
  sudo chmod -R 777 /home/ubuntu/IRIS.BCK/IRIS.BCK.Infrastructure.Files/
