090507e1c178f816555fc350cf120cb80aed121a
Omoigui Efosa <efe.omoigui@gmail.com>
2022-04-24 22:22:28 +0100

    change startup file for on develolpment settings

--- IRIS.BCK.Api/Startup.cs
+++ IRIS.BCK.Api/Startup.cs
@@ -163,12 +163,12 @@ namespace IRIS.BCK.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "IRIS.BACKEND.Api v1"));
-            }
+            //}
 
             app.UseHttpsRedirection();
             app.UseRouting();
